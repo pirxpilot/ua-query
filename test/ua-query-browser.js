64ab8fd184ea5031e23321adf31c125e7451f26d
Damian Krzeminski <pirxpilot@furkot.com>
2025-06-06 07:37:37 -0600

    use `biome` as a linter and formatter

--- test/ua-query-browser.js
+++ test/ua-query-browser.js
@@ -18,237 +18,145 @@ function set(ua) {
 describe('ua query browser', function () {
   it('should not detect Windows XP if user-agent is missing', function () {
     set('');
-    should(
-      ua.isWindowsXP()
-    ).not.be.ok();
+    should(ua.isWindowsXP()).not.be.ok();
   });
 
   it('should not detect Windows XP on Linux or MAC', function () {
     set('linux-firefox-41');
-    should(
-      ua.isWindowsXP()
-    ).not.be.ok();
+    should(ua.isWindowsXP()).not.be.ok();
 
     set('safari-7');
-    should(
-      ua.isWindowsXP()
-    ).not.be.ok();
+    should(ua.isWindowsXP()).not.be.ok();
   });
 
   it('should detect Windows XP on Windows XP', function () {
     set('windows-xp-chrome-35');
-    should(
-      ua.isWindowsXP()
-    ).be.ok();
+    should(ua.isWindowsXP()).be.ok();
   });
 
   it('should not detect Chrome if user-agent is missing', function () {
     set();
-    should(
-      ua.isChrome()
-    ).not.be.ok();
+    should(ua.isChrome()).not.be.ok();
   });
 
   it('should not detect Chrome if other browsers', function () {
     set('linux-firefox-41');
-    should(
-      ua.isChrome()
-    ).not.be.ok();
+    should(ua.isChrome()).not.be.ok();
 
     set('windows-edge-12');
-    should(
-      ua.isChrome()
-    ).not.be.ok();
+    should(ua.isChrome()).not.be.ok();
 
     set('windows-edge-79');
-    should(
-      ua.isChrome()
-    ).not.be.ok();
+    should(ua.isChrome()).not.be.ok();
 
     set('safari-7');
-    should(
-      ua.isChrome()
-    ).not.be.ok();
+    should(ua.isChrome()).not.be.ok();
 
     set('chrome ios');
-    should(
-      ua.isChrome()
-    ).not.be.ok();
+    should(ua.isChrome()).not.be.ok();
   });
 
   it('should detect Chrome', function () {
     set('windows-xp-chrome-35');
-    should(
-      ua.isChrome()
-    ).be.ok();
+    should(ua.isChrome()).be.ok();
     set('chromium');
-    should(
-      ua.isChrome()
-    ).be.ok();
+    should(ua.isChrome()).be.ok();
   });
 
   it('ua should detect Chrome iOS', function () {
     set('chrome ios');
-    should(
-      ua.isChrome_iOS()
-    ).be.ok();
+    should(ua.isChrome_iOS()).be.ok();
   });
 
   it('should detect Firefox', function () {
     set('linux-firefox-41');
-    should(
-      ua.isFirefox()
-    ).be.ok();
+    should(ua.isFirefox()).be.ok();
   });
 
   it('should detect Safari', function () {
     set('safari-7');
 
-    should(
-      ua.isSafari()
-    ).be.ok();
-    should(
-      ua.isSafari(6)
-    ).be.ok();
-    should(
-      ua.isSafari(7)
-    ).be.ok();
-    should(
-      ua.isOld('safari', 8)
-    ).be.ok();
+    should(ua.isSafari()).be.ok();
+    should(ua.isSafari(6)).be.ok();
+    should(ua.isSafari(7)).be.ok();
+    should(ua.isOld('safari', 8)).be.ok();
 
     set('mobile-safari-10');
-    should(
-      ua.isSafari(10)
-    ).be.ok();
+    should(ua.isSafari(10)).be.ok();
   });
 
   it('should not detect Safari if other browsers', function () {
     set('mac-chrome-41');
-    should(
-      ua.isSafari()
-    ).not.be.ok();
+    should(ua.isSafari()).not.be.ok();
     set('windows-xp-chrome-35');
-    should(
-      ua.isSafari()
-    ).not.be.ok();
+    should(ua.isSafari()).not.be.ok();
     set('linux-firefox-41');
-    should(
-      ua.isSafari()
-    ).not.be.ok();
+    should(ua.isSafari()).not.be.ok();
     set('windows-edge-12');
-    should(
-      ua.isSafari()
-    ).not.be.ok();
+    should(ua.isSafari()).not.be.ok();
     set('windows-edge-79');
-    should(
-      ua.isSafari()
-    ).not.be.ok();
+    should(ua.isSafari()).not.be.ok();
     set('standalone-ios');
-    should(
-      ua.isSafari()
-    ).not.be.ok();
+    should(ua.isSafari()).not.be.ok();
   });
 
   it('should not detect Firefox if Chrome', function () {
     set('windows-xp-chrome-35');
-    should(
-      ua.isFirefox()
-    ).not.be.ok();
+    should(ua.isFirefox()).not.be.ok();
   });
 
   it('should detect Chrome if requested version is the same', function () {
     set('windows-xp-chrome-35');
-    should(
-      ua.isChrome(35)
-    ).be.ok();
+    should(ua.isChrome(35)).be.ok();
     set('chromium');
-    should(
-      ua.isChrome(38)
-    ).be.ok();
+    should(ua.isChrome(38)).be.ok();
   });
 
   it('should not detect Chrome if requested version is above what we have', function () {
-    set(
-      'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36'
-    );
-    should(
-      ua.isChrome(36)
-    ).not.be.ok();
+    set('Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36');
+    should(ua.isChrome(36)).not.be.ok();
   });
 
   it('should detect IE <= 10', function () {
     set('windows-ie-9');
-    should(
-      ua.isIE()
-    ).be.ok();
+    should(ua.isIE()).be.ok();
     set('windows-ie-10');
-    should(
-      ua.isIE(10)
-    ).be.ok();
-    should(
-      ua.isIE(11)
-    ).be.not.ok();
+    should(ua.isIE(10)).be.ok();
+    should(ua.isIE(11)).be.not.ok();
   });
 
   it('should detect IE 11', function () {
     set('windows-ie-11');
-    should(
-      ua.isIE()
-    ).be.ok();
-    should(
-      ua.isIE(10)
-    ).be.ok();
-    should(
-      ua.isIE(11)
-    ).be.ok();
+    should(ua.isIE()).be.ok();
+    should(ua.isIE(10)).be.ok();
+    should(ua.isIE(11)).be.ok();
   });
 
   it('should detect Edge', function () {
     set('windows-edge-12');
-    should(
-      ua.isEdge()
-    ).be.ok();
-    should(
-      ua.isEdge(12)
-    ).be.ok();
+    should(ua.isEdge()).be.ok();
+    should(ua.isEdge(12)).be.ok();
     set('windows-edge-79');
-    should(
-      ua.isEdge()
-    ).be.ok();
-    should(
-      ua.isEdge(79)
-    ).be.ok();
+    should(ua.isEdge()).be.ok();
+    should(ua.isEdge(79)).be.ok();
     set('windows-ie-11');
-    should(
-      ua.isEdge()
-    ).be.not.ok();
+    should(ua.isEdge()).be.not.ok();
   });
 
   it('should detect standalone iOS', function () {
     set('standalone-ios');
-    should(
-      ua.isStandalone_iOS(10)
-    ).be.ok();
+    should(ua.isStandalone_iOS(10)).be.ok();
 
-    should(
-      ua.isStandalone_iOS(11)
-    ).not.be.ok();
+    should(ua.isStandalone_iOS(11)).not.be.ok();
 
     set('standalone-ios-10.1');
-    should(
-      ua.isStandalone_iOS(10)
-    ).be.ok();
+    should(ua.isStandalone_iOS(10)).be.ok();
 
     set('mobile-safari-10');
-    should(
-      ua.isStandalone_iOS()
-    ).not.be.ok();
+    should(ua.isStandalone_iOS()).not.be.ok();
 
     set('chrome ios');
-    should(
-      ua.isStandalone_iOS()
-    ).not.be.ok();
+    should(ua.isStandalone_iOS()).not.be.ok();
   });
 
   it('should check for old browser version', function () {
@@ -262,5 +170,4 @@ describe('ua query browser', function () {
     should(ua.isOld('ie', 36)).not.be.ok();
     should(ua.isOld('firefox', 36)).not.be.ok();
   });
-
 });
--- test/ua-query.js
+++ test/ua-query.js
@@ -5,121 +5,70 @@ const UA = require('./ua-strings.js');
 
 function r(ua) {
   return {
-    'headers': {
+    headers: {
       'user-agent': UA[ua] || ua
     }
   };
 }
 
 describe('ua query', function () {
-
   it('should not detect Windows XP if user-agent is missing', function () {
-    should(
-      ua.isWindowsXP(r(''))
-    ).not.be.ok();
+    should(ua.isWindowsXP(r(''))).not.be.ok();
   });
 
   it('should not detect Windows XP on Linux or MAC', function () {
-    should(
-      ua.isWindowsXP(r('linux-firefox-41'))
-    ).not.be.ok();
-    should(
-      ua.isWindowsXP(r('safari-7'))
-    ).not.be.ok();
+    should(ua.isWindowsXP(r('linux-firefox-41'))).not.be.ok();
+    should(ua.isWindowsXP(r('safari-7'))).not.be.ok();
   });
 
   it('should detect Windows XP on Windows XP', function () {
-    should(
-      ua.isWindowsXP(r('windows-xp-chrome-35'))
-    ).be.ok();
+    should(ua.isWindowsXP(r('windows-xp-chrome-35'))).be.ok();
   });
 
   it('should not detect Chrome if user-agent is missing', function () {
-    should(
-      ua.isChrome(r())
-    ).not.be.ok();
+    should(ua.isChrome(r())).not.be.ok();
   });
 
   it('should not detect Chrome if other browsers', function () {
-    should(
-      ua.isChrome(r('linux-firefox-41'))
-    ).not.be.ok();
+    should(ua.isChrome(r('linux-firefox-41'))).not.be.ok();
 
-    should(
-      ua.isChrome(r('windows-edge-12'))
-    ).not.be.ok();
+    should(ua.isChrome(r('windows-edge-12'))).not.be.ok();
 
-    should(
-      ua.isChrome(r('windows-edge-79'))
-    ).not.be.ok();
+    should(ua.isChrome(r('windows-edge-79'))).not.be.ok();
 
-    should(
-      ua.isChrome(r('safari-7'))
-    ).not.be.ok();
+    should(ua.isChrome(r('safari-7'))).not.be.ok();
 
-    should(
-      ua.isChrome(r('chrome ios'))
-    ).not.be.ok();
+    should(ua.isChrome(r('chrome ios'))).not.be.ok();
   });
 
   it('should detect Chrome', function () {
-    should(
-      ua.isChrome(r('windows-xp-chrome-35'))
-    ).be.ok();
-    should(
-      ua.isChrome(r('chromium'))
-    ).be.ok();
+    should(ua.isChrome(r('windows-xp-chrome-35'))).be.ok();
+    should(ua.isChrome(r('chromium'))).be.ok();
   });
 
   it('ua should detect Chrome iOS', function () {
-    should(
-      ua.isChrome_iOS(r('chrome ios'))
-    ).be.ok();
+    should(ua.isChrome_iOS(r('chrome ios'))).be.ok();
   });
 
   it('should detect Firefox', function () {
-    should(
-      ua.isFirefox(r('linux-firefox-41'))
-    ).be.ok();
+    should(ua.isFirefox(r('linux-firefox-41'))).be.ok();
   });
 
   it('should detect Safari', function () {
-    should(
-      ua.isSafari(r('safari-7'))
-    ).be.ok();
-    should(
-      ua.isSafari(r('safari-7', 6))
-    ).be.ok();
-    should(
-      ua.isSafari(r('safari-7', 7))
-    ).be.ok();
-    should(
-      ua.isSafari(r('mobile-safari-10'), 10)
-    ).be.ok();
-    should(
-      ua.isOld(r('safari-7'), 'safari', 8)
-    ).be.ok();
+    should(ua.isSafari(r('safari-7'))).be.ok();
+    should(ua.isSafari(r('safari-7', 6))).be.ok();
+    should(ua.isSafari(r('safari-7', 7))).be.ok();
+    should(ua.isSafari(r('mobile-safari-10'), 10)).be.ok();
+    should(ua.isOld(r('safari-7'), 'safari', 8)).be.ok();
   });
 
   it('should not detect Safari if other browsers', function () {
-    should(
-      ua.isSafari(r('mac-chrome-41'))
-    ).not.be.ok();
-    should(
-      ua.isSafari(r('windows-xp-chrome-35'))
-    ).not.be.ok();
-    should(
-      ua.isSafari(r('linux-firefox-41'))
-    ).not.be.ok();
-    should(
-      ua.isSafari(r('windows-edge-12'))
-    ).not.be.ok();
-    should(
-      ua.isSafari(r('windows-edge-79'))
-    ).not.be.ok();
-    should(
-      ua.isSafari(r('standalone-ios'))
-    ).not.be.ok();
+    should(ua.isSafari(r('mac-chrome-41'))).not.be.ok();
+    should(ua.isSafari(r('windows-xp-chrome-35'))).not.be.ok();
+    should(ua.isSafari(r('linux-firefox-41'))).not.be.ok();
+    should(ua.isSafari(r('windows-edge-12'))).not.be.ok();
+    should(ua.isSafari(r('windows-edge-79'))).not.be.ok();
+    should(ua.isSafari(r('standalone-ios'))).not.be.ok();
   });
 
   it('should parse headers once', function () {
@@ -133,74 +82,43 @@ describe('ua query', function () {
   });
 
   it('should not detect Firefox if Chrome', function () {
-    should(
-      ua.isFirefox(r('windows-xp-chrome-35'))
-    ).not.be.ok();
+    should(ua.isFirefox(r('windows-xp-chrome-35'))).not.be.ok();
   });
 
   it('should detect Chrome if requested version is the same', function () {
-    should(
-      ua.isChrome(r('windows-xp-chrome-35'), 35)
-    ).be.ok();
-    should(
-      ua.isChrome(r('chromium'), 38)
-    ).be.ok();
-    should(
-      ua.isChrome(r('chrome-100'), 100)
-    ).be.ok();
-    should(
-      ua.isChrome(r('android-chrome-reduced'), 93)
-    ).be.ok();
+    should(ua.isChrome(r('windows-xp-chrome-35'), 35)).be.ok();
+    should(ua.isChrome(r('chromium'), 38)).be.ok();
+    should(ua.isChrome(r('chrome-100'), 100)).be.ok();
+    should(ua.isChrome(r('android-chrome-reduced'), 93)).be.ok();
   });
 
   it('should not detect Chrome if requested version is above what we have', function () {
     should(
-      ua.isChrome(r(
-        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36'
-      ), 36)
+      ua.isChrome(
+        r('Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36'),
+        36
+      )
     ).not.be.ok();
   });
 
   it('should detect IE <= 10', function () {
-    should(
-      ua.isIE(r('windows-ie-9'))
-    ).be.ok();
-    should(
-      ua.isIE(r('windows-ie-10'), 10)
-    ).be.ok();
-    should(
-      ua.isIE(r('windows-ie-10'), 11)
-    ).be.not.ok();
+    should(ua.isIE(r('windows-ie-9'))).be.ok();
+    should(ua.isIE(r('windows-ie-10'), 10)).be.ok();
+    should(ua.isIE(r('windows-ie-10'), 11)).be.not.ok();
   });
 
   it('should detect IE 11', function () {
-    should(
-      ua.isIE(r('windows-ie-11'))
-    ).be.ok();
-    should(
-      ua.isIE(r('windows-ie-11'), 10)
-    ).be.ok();
-    should(
-      ua.isIE(r('windows-ie-11'), 11)
-    ).be.ok();
+    should(ua.isIE(r('windows-ie-11'))).be.ok();
+    should(ua.isIE(r('windows-ie-11'), 10)).be.ok();
+    should(ua.isIE(r('windows-ie-11'), 11)).be.ok();
   });
 
   it('should detect Edge', function () {
-    should(
-      ua.isEdge(r('windows-edge-12'))
-    ).be.ok();
-    should(
-      ua.isEdge(r('windows-edge-12'), 12)
-    ).be.ok();
-    should(
-      ua.isEdge(r('windows-edge-79'))
-    ).be.ok();
-    should(
-      ua.isEdge(r('windows-edge-79'), 79)
-    ).be.ok();
-    should(
-      ua.isEdge(r('windows-ie-11'))
-    ).be.not.ok();
+    should(ua.isEdge(r('windows-edge-12'))).be.ok();
+    should(ua.isEdge(r('windows-edge-12'), 12)).be.ok();
+    should(ua.isEdge(r('windows-edge-79'))).be.ok();
+    should(ua.isEdge(r('windows-edge-79'), 79)).be.ok();
+    should(ua.isEdge(r('windows-ie-11'))).be.not.ok();
   });
 
   it('should check for old browser version', function () {
@@ -216,66 +134,37 @@ describe('ua query', function () {
   });
 
   it('should detect bot for BingPreview', function () {
-    should(
-      ua.isBot(r('bing'))
-    ).be.ok();
-    should(
-      ua.isBot(r('mac-chrome-41'))
-    ).not.be.ok();
+    should(ua.isBot(r('bing'))).be.ok();
+    should(ua.isBot(r('mac-chrome-41'))).not.be.ok();
   });
 
   it('should detect android', function () {
-    should(
-      ua.isAndroid(r('android-firefox'))
-    ).be.ok();
-    should(
-      ua.isAndroid(r('android-chrome'))
-    ).be.ok();
-    should(
-      ua.isAndroid(r('android-chrome-reduced'))
-    ).be.ok();
+    should(ua.isAndroid(r('android-firefox'))).be.ok();
+    should(ua.isAndroid(r('android-chrome'))).be.ok();
+    should(ua.isAndroid(r('android-chrome-reduced'))).be.ok();
   });
 
   it('should not detect android if other OSes', function () {
-    should(
-      ua.isAndroid(r('mobile-safari-10'))
-    ).not.be.ok();
-    should(
-      ua.isAndroid(r('windows-ie-11'))
-    ).not.be.ok();
+    should(ua.isAndroid(r('mobile-safari-10'))).not.be.ok();
+    should(ua.isAndroid(r('windows-ie-11'))).not.be.ok();
   });
 
   it('should detect iOS', function () {
-    should(
-      ua.is_iOS(r('mobile-safari-10'))
-    ).be.ok();
-    should(
-      ua.is_iOS(r('standalone-ios'))
-    ).be.ok();
+    should(ua.is_iOS(r('mobile-safari-10'))).be.ok();
+    should(ua.is_iOS(r('standalone-ios'))).be.ok();
   });
 
   it('should not detect iOS if other OSes', function () {
-    should(
-      ua.is_iOS(r('android-chrome'))
-    ).not.be.ok();
-    should(
-      ua.is_iOS(r('edge-mobile'))
-    ).not.be.ok();
+    should(ua.is_iOS(r('android-chrome'))).not.be.ok();
+    should(ua.is_iOS(r('edge-mobile'))).not.be.ok();
   });
 
   it('should detect Windows Phone', function () {
-    should(
-      ua.isWindowsPhone(r('edge-mobile'))
-    ).be.ok();
+    should(ua.isWindowsPhone(r('edge-mobile'))).be.ok();
   });
 
   it('should not detect Windows Phone if other OSes', function () {
-    should(
-      ua.isWindowsPhone(r('windows-edge-12'))
-    ).not.be.ok();
-    should(
-      ua.isWindowsPhone(r('windows-edge-79'))
-    ).not.be.ok();
+    should(ua.isWindowsPhone(r('windows-edge-12'))).not.be.ok();
+    should(ua.isWindowsPhone(r('windows-edge-79'))).not.be.ok();
   });
-
 });
